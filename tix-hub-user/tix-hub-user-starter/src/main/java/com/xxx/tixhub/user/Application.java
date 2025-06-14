bd4e6281ff5f5a60969c3a11f60947863c2cdca3
wanggwang <gangwang1014@163.com>
2025-06-14 14:45:40 +0800

    feat(redis): 增加完善redis-starter

---
+++ tix-hub-user/tix-hub-user-starter/src/main/java/com/xxx/tixhub/user/Application.java
@@ -0,0 +1,17 @@
+package com.xxx.tixhub.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author by 红叶已尽秋
+ * @classname Application
+ * @description Application
+ * @date 2025/5/29 23:28
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
